b8425b96f0f3b7b729b05dd28781ae87d93d3669
D1m11a <128309725+D1m11a@users.noreply.github.com>
2023-07-27 16:52:52 +0300

    Update Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class Main {
+public static void main(String[\] args) {
+Editor.printShapeName(new Ellipse());
+Editor.printShapeName(new Circle());
+Editor.printShapeName(new Quad());
+Editor.printShapeName(new Rectangle());
+Editor.printShapeName(new Triangle());
+}
+}"
